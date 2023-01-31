f911df6e83a5f23a2078886b5daa83a3b51918a0
Marcinho <marcio.mrcr@outlook.com>
2023-01-31 15:49:48 -0300

    created queries sql

--- src/routes.ts
+++ src/routes.ts
@@ -74,6 +74,32 @@ export async function appRoutes(app: FastifyInstance) {
     };
   });
 
+  app.get('/summary', async (request) => {
+    const summary = await prisma.$queryRaw`
+    SELECT 
+    D.id,
+    D.date,
+    (
+      SELECT 
+      cast(count(*) as float)
+      FROM day_habits DH
+      WHERE DH.day_id = D.id
+    ) as completed,
+    (
+      SELECT 
+      cast(count(*) as float)
+      FROM habit_week_days HWD
+      JOIN habits H
+      ON H.id = HWD.habit_id
+      WHERE 
+      HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+      AND H.created_at <= D.date
+    ) as amount
+     FROM days D
+    `;
+    return summary;
+  });
+
   //marcar ou desmarcar como concluído
   app.patch('/habits/:id/toggle', async (request) => {
     const toggleHabitParams = z.object({
